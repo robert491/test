d5cd65bc9b6ddd2a3cdd5f37f3b74c5fec84eb80
robert491 <bob.pototsky@gmail.com>
2020-10-21 16:20:19 +0300

    fix duplicated Math.sqrt()

--- lesson1/quadratic.rb
+++ lesson1/quadratic.rb
@@ -6,20 +6,21 @@ numbers = []
 end
 
 a, b, c = numbers
-d = b ** 2 - 4 * a * c
+discriminant = b ** 2 - 4 * a * c
+discriminant_root = Math.sqrt(discriminant)
 
-unless d.negative?
-  first_x = (- b + Math.sqrt(d)) / (2 * a) 
-  second_x = (- b - Math.sqrt(d)) / (2 * a)
+unless discriminant.negative?
+  first_x = (- b + discriminant_root) / (2 * a) 
+  second_x = (- b - discriminant_root) / (2 * a)
 end
 
 message =
-  if d.positive?
-    "D = #{d}. x1 = #{first_x}, x2 = #{second_x}."
+  if discriminant.positive?
+    "D = #{discriminant}. x1 = #{first_x}, x2 = #{second_x}."
   elsif d.zero?
-    "D = #{d}. x = #{first_x}."
+    "D = #{discriminant}. x = #{first_x}."
   else
-    "D = #{d}. Корней нет."
+    "D = #{discriminant}. Корней нет."
   end
 
 puts message
