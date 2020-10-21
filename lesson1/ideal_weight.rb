5c264eda0b28fdb5dc82f079c5f2588280bd379f
robert491 <bob.pototsky@gmail.com>
2020-10-21 16:13:23 +0300

    replaced .round to the variable initialization

--- lesson1/ideal_weight.rb
+++ lesson1/ideal_weight.rb
@@ -4,13 +4,13 @@ user_name = gets.chomp
 puts "Ваш рост:"
 user_height = gets.to_f
 
-ideal_weight = (user_height - 110) * 1.15
+ideal_weight = ((user_height - 110) * 1.15).round
 
 message =
   if ideal_weight.negative?
     "Ваш вес уже оптимальный."
   else
-    "#{user_name}, ваш идеальный вес #{ideal_weight.round}."
+    "#{user_name}, ваш идеальный вес #{ideal_weight}."
   end
 
 puts message
