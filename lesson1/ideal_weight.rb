00ebf65e06a782c266b13617b87e33ead798c67b
robert491 <bob.pototsky@gmail.com>
2020-10-21 16:09:22 +0300

    replace .round to the message

--- lesson1/ideal_weight.rb
+++ lesson1/ideal_weight.rb
@@ -5,13 +5,12 @@ puts "Ваш рост:"
 user_height = gets.to_f
 
 ideal_weight = (user_height - 110) * 1.15
-ideal_weight.round
 
 message =
   if ideal_weight.negative?
     "Ваш вес уже оптимальный."
   else
-    "#{user_name}, ваш идеальный вес #{ideal_weight}."
+    "#{user_name}, ваш идеальный вес #{ideal_weight.round}."
   end
 
 puts message
