b15cae5cdd66287d8f908f27a2c46963f94890a8
robert491 <bob.pototsky@gmail.com>
2020-10-22 15:29:19 +0300

    first commit

---
+++ lesson2/leap_year.rb
@@ -0,0 +1,44 @@
+common_year = {
+  1 =>  31,
+  2 =>  28,
+  3 =>  31,
+  4 =>  30,
+  5 =>  31,
+  6 =>  30,
+  7 =>  31,
+  8 =>  31,
+  9 =>  30,
+  10 => 31,
+  11 => 30,
+  12 => 31 
+}
+
+def leap?(year)
+  if (year % 4).positive?
+    false
+  elsif (year % 100).positive?
+    true
+  elsif (year % 400).positive?
+    false
+  else
+    true
+  end
+end
+
+print "Введите число: "
+user_date = gets.to_i
+
+print "Введите месяц (1-12): "
+user_month = gets.to_i
+
+print "Введите год: "
+user_year = gets.to_i
+
+quantity = leap?(user_year) && user_month > 2 ? user_date += 1 : user_date
+
+common_year.each do |month, days|
+  break if month == user_month
+  quantity += days
+end
+
+puts quantity
