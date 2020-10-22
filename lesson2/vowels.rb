9fb3428358db6102f8000265b4f3ca92e85781e8
robert491 <bob.pototsky@gmail.com>
2020-10-23 01:11:35 +0300

    remove +1

--- lesson2/vowels.rb
+++ lesson2/vowels.rb
@@ -2,4 +2,5 @@ abc = ('a'..'z').to_a
 vowels = %w[a e i o u y]
 
 vowels_index = {}
-vowels.each { |vowel| vowels_index[vowel] = abc.index(vowel) + 1 }
+
+abc.each.with_index(1) { |letter, index| vowels_index[letter] = index if vowels.include?(letter) }
