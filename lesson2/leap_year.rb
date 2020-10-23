522b75fb99a780c427589002110298e5e70539ed
robert491 <bob.pototsky@gmail.com>
2020-10-23 17:03:38 +0300

    added .take and .sum

--- lesson2/leap_year.rb
+++ lesson2/leap_year.rb
@@ -1,17 +1,4 @@
-common_year = {
-  1 =>  31,
-  2 =>  28,
-  3 =>  31,
-  4 =>  30,
-  5 =>  31,
-  6 =>  30,
-  7 =>  31,
-  8 =>  31,
-  9 =>  30,
-  10 => 31,
-  11 => 30,
-  12 => 31 
-}
+common_year = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 def leap?(year)
   if (year % 4).positive?
@@ -36,8 +23,6 @@ user_year = gets.to_i
 
 quantity = leap?(user_year) && user_month > 2 ? user_date += 1 : user_date
 
-(1...user_month).each do |month|
-  quantity += common_year[month]
-end
+quantity += common_year.take(user_month - 1).sum
 
 puts quantity
